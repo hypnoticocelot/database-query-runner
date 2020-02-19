146606fa8b87d5767bcc7b79b4fe30e8ea2c7336
Ryan Kennedy <rckenned@gmail.com>
2020-02-18 21:04:08 -0800

    Initial Maven project creation
    Includes a base POM file and the QueryRunnerMain class as
    the entrypoint for the forthcoming Docker image.

---
+++ src/main/java/com/hypnoticocelot/database/QueryRunnerMain.java
@@ -0,0 +1,7 @@
+package com.hypnoticocelot.database;
+
+public class QueryRunnerMain {
+    public static void main(String[] args) {
+        System.err.println("Hello, World! I'm not implemented, yet!");
+    }
+}
